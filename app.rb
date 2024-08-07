1a201e91e6e4fc223d9ad66fe1dd2d5157a7a501
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-07 15:14:00 -0400

    Refactored

--- app.rb
+++ app.rb
@@ -7,14 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/team' do
-    name = params[:name]
-    coach = params[:coach]
-    pg = params[:pg]
-    sg = params[:sg]
-    sf = params[:sf]
-    pf = params[:pf]
-    c = params[:c]
-    erb :team, locals: { name: name, coach: coach, pg: pg, sg: sg, sf: sf, pf: pf, c: c}
+    erb :team
   end
 
 end
